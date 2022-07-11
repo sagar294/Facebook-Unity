dd33a9ba67a731fa208066798bb06fa9f5cacccf
spatel210 <psajan210@gmail.com>
2022-07-11 18:18:16 +0530

    Code added

---
+++ Assets/Scripts/FacebookManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+
+public class FacebookManager : MonoBehaviour
+{
+    public Text FriendsText;
+
+    private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                    FB.ActivateApp();
+                else
+                    Debug.LogError("Couldn't initialize");
+            },
+            isGameShown =>
+            {
+                if (!isGameShown)
+                    Time.timeScale = 0;
+                else
+                    Time.timeScale = 1;
+            });
+        }
+        else
+            FB.ActivateApp();
+    }
+
+    #region Login / Logout
+    public void FacebookLogin()
+    {
+        var permissions = new List<string>() { "public_profile", "email", "user_friends" };
+        FB.LogInWithReadPermissions(permissions, AuthCallback);
+    }
+
+    private void AuthCallback(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+            Debug.Log(aToken.UserId);
+            foreach (string perm in aToken.Permissions)
+            {
+                Debug.Log(perm);
+                FriendsText.text += perm + "    ";
+            }
+        }
+        else
+        {
+            FriendsText.text = "User cancelled login";
+            Debug.Log("User cancelled login");
+        }
+    }
+    public void FacebookLogout()
+    {
+        FB.LogOut();
+    }
+    #endregion
+
+    public void FacebookShare()
+    {
+        FB.ShareLink(new System.Uri("https://resocoder.com"), "Check it out!",
+            "Good programming tutorials lol!",
+            new System.Uri("https://resocoder.com/wp-content/uploads/2017/01/logoRound512.png"));
+    }
+
+    #region Inviting
+    public void FacebookGameRequest()
+    {
+        FB.AppRequest("Hey! Come and play this awesome game!", title: "Reso Coder Tutorial");
+    }
+
+    public void FacebookInvite()
+    {
+        //FB.Mobile.AppInvite(new System.Uri("https://play.google.com/store/apps/details?id=com.tappybyte.byteaway"));
+    }
+    #endregion
+
+    public void GetFriendsPlayingThisGame()
+    {
+        string query = "/me/friends";
+        FB.API(query, HttpMethod.GET, result =>
+        {
+            var dictionary = (Dictionary<string, object>)Facebook.MiniJSON.Json.Deserialize(result.RawResult);
+            var friendsList = (List<object>)dictionary["data"];
+            FriendsText.text = "" + friendsList.Count;
+            //foreach (var dict in friendsList)
+                //FriendsText.text += ((Dictionary<string, object>)dict)["name"];
+        });
+    }
+}
